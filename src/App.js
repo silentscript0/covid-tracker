73b58fc95ed6088bf3c297f7150d4d7c606c3f6d
TomirisAndarbayeva <andarbayeva.tomiris@gmail.com>
2021-06-18 20:00:45 +0600

    final

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import { fetchData } from './API';
+import coronaImage from './images/image.png';
+import { Cards, Chart, CountryPicker } from './components';
+
+class App extends Component {
+  state = {
+    data: {},
+    country: '',
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    console.log(fetchedData);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="CORONA VIRUS" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// exporting all of the components overhere not to mess up with other imports in App.js
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './Country/Country';
